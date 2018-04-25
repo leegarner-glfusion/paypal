55d74ee79b8f2de194d10cfb6892599fb23929fd
Lee Garner <lee@leegarner.com>
2018-04-25 09:45:30 -0700

    Work with new cart getInstance and args array

--- public_html/ajax.php
+++ public_html/ajax.php
@@ -22,7 +22,8 @@ switch ($_GET['action']) {
 case 'delAddress':          // Remove a shipping address
     if ($uid < 2) break;    // Not available to anonymous
     $id = (int)$_GET['id']; // Sanitize address ID
-    DB_delete($_TABLES['paypal.address'], array('id','uid'), array($id,$uid));
+    Paypal\UserInfo::deleteAddress($id);
+    //DB_delete($_TABLES['paypal.address'], array('id','uid'), array($id,$uid));
     break;
 case 'getAddress':
     if ($uid < 2) break;
@@ -35,19 +36,26 @@ case 'getAddress':
 case 'addcartitem':
     if (!isset($_POST['item_number'])) {
         echo json_encode(array('content' => '', 'statusMessage' => ''));;
-         exit;
+        exit;
     }
-    PAYPAL_setCart();
+    //PAYPAL_setCart();
+    $ppGCart = Paypal\Cart::getInstance();
     if (isset($_POST['_unique']) && $_POST['_unique'] &&
             $ppGCart->Contains($_POST['item_number']) !== false) {
         break;
     }
-    $qty = isset($_POST['quantity']) ? (float)$_POST['quantity'] : 1;
-    $options = isset($_POST['options']) ? $_POST['options'] : array();
-    $extras = isset($_POST['extras']) ? $_POST['extras'] : array();
-    $ppGCart->addItem($_POST['item_number'], $_POST['item_name'],
-                $_POST['item_descr'], $qty,
-                $_POST['base_price'], $options, $extras);
+    $args = array(
+        'item_number' => isset($_POST['item_number']) ? $_POST['item_number'] : '',
+        'item_name' => isset($_POST['item_name']) ? $_POST['item_name'] : '',
+        'description' => isset($_POST['item_descr']) ? $_POST['item_descr'] : '',
+        'quantity' => isset($_POST['quantity']) ? (float)$_POST['quantity'] : 1,
+        'price' => isset($_POST['base_price']) ? $_POST['base_price'] : 0,
+        'options' => isset($_POST['options']) ? $_POST['options'] : array(),
+        'extras' => isset($_POST['extras']) ? $_POST['extras'] : array(),
+        //'tax' => isset($_POST['tax']) ? $_POST['tax'] : NULL,
+    );
+
+    $ppGCart->addItem($args);
     $A = array(
         'content' => phpblock_paypal_cart_contents(),
         'statusMessage' => $LANG_PP['msg_item_added'],
