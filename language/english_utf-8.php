33f442a4bc47b1dafdd466cacb492abf66e565d0
Lee Garner <lee@leegarner.com>
2018-10-18 16:30:18 -0700

    Merge fixup

--- language/english_utf-8.php
+++ language/english_utf-8.php
@@ -391,7 +391,6 @@ $LANG_PP = array (
 'dscp_purge_carts' => 'Delete all customer shopping carts that have not been finalized as orders.<br /><b>This will impact the guest experience and should not normally be necessary.</b>',
 'include' => 'Include',
 'exclude' => 'Exclude',
-<<<<<<< HEAD
 'dscp_change_currency' => 'Change prices for products, attributes and shopping cart items to a new currency.<br /><b>This will adjust product prices. You should review all prices for accuracy following this operation.</b>',
 'change_currency' => 'Change Currency',
 'old_currency' => 'Old Currency Setting',
@@ -399,10 +398,7 @@ $LANG_PP = array (
 'q_change_currency' => 'Are you sure you want to change prices and other amounts based on the new currency?',
 'cur_changed' => 'Prices changed from %1$s to %2%s',
 'no_cur_change' => 'No currency conversion was done.',
-'buy_x_save' => 'Buy %1$d, save %2$.02f%%',
-=======
 'buy_x_save' => 'Buy %1$d, save %2$s%%',
->>>>>>> develop
 'out_of_stock' => 'This item is currently out of stock.',
 'shipping_method' => 'Shipping Method',
 'shipping_units' => 'Shipping Units',
