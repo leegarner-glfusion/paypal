45a7033dab2ef4c06484018a3619356f8e5dd50f
Lee Garner <lee@leegarner.com>
2019-01-09 14:41:28 -0800

    Prevent dvlpudate from running if plugin is not enabled

--- admin/dvlpupdate.php
+++ admin/dvlpupdate.php
@@ -9,7 +9,7 @@
  * @author      Lee Garner <lee@leegarner.com>
  * @copyright   Copyright (c) 2018 Lee Garner <lee@leegarner.com>
  * @package     paypal
- * @version     v0.6.0
+ * @version     v0.6.1
  * @since       v0.6.0
  * @license     http://opensource.org/licenses/gpl-2.0.php
  *              GNU Public License v2 or later
@@ -17,6 +17,13 @@
  */
 
 require_once '../../../lib-common.php';
+
+// If plugin is not installed or not enabled, display an error and exit gracefully
+if (!isset($_PP_CONF) || !in_array($_PP_CONF['pi_name'], $_PLUGINS)) {
+    echo "This plugin is either not installed or not enabled.";
+    exit;
+}
+
 if (!SEC_inGroup('Root')) {
     // Someone is trying to illegally access this page
     COM_errorLog("Someone has tried to access the Paypal Development Code Upgrade Routine without proper permissions.  User id: {$_USER['uid']}, Username: {$_USER['username']}, IP: " . $_SERVER['REMOTE_ADDR'],1);
@@ -24,6 +31,7 @@ if (!SEC_inGroup('Root')) {
     exit;
 }
 require_once PAYPAL_PI_PATH . '/upgrade.inc.php';   // needed for set_version()
+
 if (function_exists('CACHE_clear')) {
     CACHE_clear();
 }
